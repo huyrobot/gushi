66412df29f77dae2099208f7349d0a2ef42bafe0
LeQuangHuy <huyrobotx@gmail.com>
2022-05-10 22:17:41 +0700

    fist

---
+++ Gushi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gushi
+//
+//  Created by CNTT on 5/10/22.
+//  Copyright © 2022 fit.tdc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
